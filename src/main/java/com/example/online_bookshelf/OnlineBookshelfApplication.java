a95211544c9f9a2d128b0cab30af87985a4601d5
Nadia <nadia_dj@hotmail.co.uk>
2022-12-12 09:13:47 +0000

    initialise repo and add diagrams

---
+++ src/main/java/com/example/online_bookshelf/OnlineBookshelfApplication.java
@@ -0,0 +1,13 @@
+package com.example.online_bookshelf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineBookshelfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineBookshelfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/online_bookshelf/OnlineBookshelfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.online_bookshelf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineBookshelfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
