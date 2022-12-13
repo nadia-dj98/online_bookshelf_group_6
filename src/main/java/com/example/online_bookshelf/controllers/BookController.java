17746caa763d7f1b02797587ede0967ec7a4879c
Nadia <nadia_dj@hotmail.co.uk>
2022-12-13 09:53:56 +0000

    created get request for all  books

--- src/main/java/com/example/online_bookshelf/controllers/BookController.java
+++ src/main/java/com/example/online_bookshelf/controllers/BookController.java
@@ -5,10 +5,9 @@ import com.example.online_bookshelf.services.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/books")
@@ -21,5 +20,13 @@ public class BookController {
         Book newBook = bookService.addBook(book);
         return new ResponseEntity<>(newBook, HttpStatus.CREATED);
     }
+
+    @GetMapping
+    public ResponseEntity<List<Book>> getAllBooks(){
+        List<Book> books = bookService.displayAllBooks();
+        return new ResponseEntity<>(books, HttpStatus.OK);
+    }
+
+
 
 }
